86d62df15e89df771c9a05713944a5e50e1e3e9e
Rockwell Schrock <rockwell@schrock.me>
2025-07-07 19:54:33 -0400

    Add /admin redirect

--- lib/veotags_web/controllers/page_controller.ex
+++ lib/veotags_web/controllers/page_controller.ex
@@ -1,14 +1,18 @@
 defmodule VeotagsWeb.PageController do
   use VeotagsWeb, :controller
 
-  def up(conn, _params) do
-    tag_count = Veotags.Mapping.count_tags()
-    text(conn, "#{tag_count} tags and counting...")
-  end
-
   def about(conn, _params) do
     conn
     |> assign(:page_title, "About")
     |> render()
+  end
+
+  def admin(conn, _params) do
+    redirect(conn, to: "/admin/tags")
+  end
+
+  def up(conn, _params) do
+    tag_count = Veotags.Mapping.count_tags()
+    text(conn, "#{tag_count} tags and counting...")
   end
 end

3d9cadbf44685076bb6fe92a01bb2145b0f0aea4
Rockwell Schrock <rockwell@schrock.me>
2025-07-08 17:19:19 -0400

    Add favicons

--- lib/veotags_web.ex
+++ lib/veotags_web.ex
@@ -17,7 +17,21 @@ defmodule VeotagsWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths do
+    [
+      "android-chrome-192x192.png",
+      "android-chrome-512x512.png",
+      "apple-touch-icon.png",
+      "assets",
+      "favicon-16x16.png",
+      "favicon-32x32.png",
+      "favicon.ico",
+      "fonts",
+      "images",
+      "robots.txt",
+      "site.webmanifest"
+    ]
+  end
 
   def router do
     quote do
